aa77cbe5bf0021914f96835b260db495b783026c
chqu1012 <chqu1012@hs-karlsruhe.de>
2013-02-20 18:18:44 +0100

    Try Project Nature & Build

--- WorkspaeExample/src/workspaceexample/Perspective.java
+++ WorkspaeExample/src/workspaceexample/Perspective.java
@@ -2,13 +2,18 @@ package workspaceexample;
 
 import org.eclipse.ui.IPageLayout;
 import org.eclipse.ui.IPerspectiveFactory;
+import org.eclipse.ui.IFolderLayout;
 
 public class Perspective implements IPerspectiveFactory {
 
 	public void createInitialLayout(IPageLayout layout) {
 		layout.setEditorAreaVisible(false);
 		layout.setFixed(true);
-		
+		{
+			IFolderLayout folderLayout = layout.createFolder("folder", IPageLayout.LEFT, 0.5f, IPageLayout.ID_EDITOR_AREA);
+			folderLayout.addView("org.eclipse.ui.navigator.ProjectExplorer");
+			folderLayout.addView("org.eclipse.jdt.ui.ProjectsView");
+		}
 	}
 
 }
