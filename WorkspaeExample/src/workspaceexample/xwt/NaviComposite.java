655aa569baf8a39c1b4f41af723167960c3b16e9
chqu1012 <chqu1012@hs-karlsruhe.de>
2013-02-20 17:54:40 +0100

    Link Testing

--- WorkspaeExample/src/workspaceexample/xwt/NaviComposite.java
+++ WorkspaeExample/src/workspaceexample/xwt/NaviComposite.java
@@ -2,15 +2,24 @@ package workspaceexample.xwt;
 
 import java.awt.Desktop;
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.io.InputStream;
 import java.net.URL;
 import java.util.HashMap;
 import java.util.Map;
 
+import org.eclipse.core.resources.IFile;
+import org.eclipse.core.resources.IFolder;
+import org.eclipse.core.resources.IPathVariableManager;
 import org.eclipse.core.resources.IProject;
+import org.eclipse.core.resources.IResource;
 import org.eclipse.core.resources.IWorkspaceRoot;
 import org.eclipse.core.resources.ResourcesPlugin;
 import org.eclipse.core.runtime.CoreException;
+import org.eclipse.core.runtime.IPath;
+import org.eclipse.core.runtime.Path;
 import org.eclipse.e4.xwt.DefaultLoadingContext;
 import org.eclipse.e4.xwt.IConstants;
 import org.eclipse.e4.xwt.IXWTLoader;
@@ -18,9 +27,11 @@ import org.eclipse.e4.xwt.XWT;
 import org.eclipse.e4.xwt.annotation.UI;
 import org.eclipse.e4.xwt.forms.XWTForms;
 import org.eclipse.jface.dialogs.MessageDialog;
+import org.eclipse.swt.SWT;
 import org.eclipse.swt.layout.FillLayout;
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.Event;
+import org.eclipse.swt.widgets.FileDialog;
 import org.eclipse.swt.widgets.Shell;
 import org.eclipse.swt.widgets.Text;
 
@@ -28,7 +39,7 @@ public class NaviComposite extends Composite {
 
 	IWorkspaceRoot workspace = ResourcesPlugin.getWorkspace().getRoot();
 	
-	@UI Text projectText, folderNameButton, countOfProjectText;
+	@UI Text projectText, folderNameButton, countOfProjectText, linkResourceText;
 
 	private IProject project;
 	
@@ -60,8 +71,9 @@ public class NaviComposite extends Composite {
 		try {
 			if(project.exists())
 				MessageDialog.openInformation(new Shell(), "Folder create", "Folder exist, please give another name!");
-			else
+			else{
 				project.create(null);
+			}
 		} catch (CoreException e) {
 			e.printStackTrace();
 		}
@@ -81,5 +93,22 @@ public class NaviComposite extends Composite {
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
+	}
+	public void onOpenLinkResourceButtonSelection(Event event) {
+		String linkResourceFilePath=new FileDialog(new Shell(), SWT.OPEN).open();
+	
+		if(linkResourceFilePath!=null)
+			linkResourceText.setText(linkResourceFilePath);
+	}
+	public void onLinkResourceButtonSelection(Event event) {
+		 IProject project = workspace.getProject(projectText.getText());//assume this exists
+		   IFolder link = project.getFolder(folderNameButton.getText());
+		   IFile child = link.getFile(linkResourceText.getText());
+		      try {
+		    	InputStream source = new FileInputStream(linkResourceText.getText());
+				child.create(source, true, null);
+			} catch (CoreException | FileNotFoundException e) {
+				e.printStackTrace();
+			}
 	}
 }
